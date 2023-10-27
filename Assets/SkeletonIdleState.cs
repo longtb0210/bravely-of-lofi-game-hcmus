ebd703cb12578ccaab9cd1a3e1b2464c0c5c1dd5
Bao <20127557@student.hcmus.edu.vn>
2023-10-27 15:18:29 +0700

    Lesson 50

--- Assets/SkeletonIdleState.cs
+++ Assets/SkeletonIdleState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SkeletonIdleState : EnemyState
 {
+
     Enemy_Skeleton enemy;
 
     public SkeletonIdleState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton _enemy) : base(_enemyBase, _stateMachine, _animBoolName)

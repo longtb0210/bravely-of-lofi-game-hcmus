a388753366ebd341463b9c6cf01160583068b1d3
Bao <20127557@student.hcmus.edu.vn>
2023-10-31 10:12:14 +0700

    Lesson 58

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -11,6 +11,11 @@ public class Entity : MonoBehaviour
     public EntityFX fx { get; private set; }
     #endregion
 
+    [Header("Knockback info")]
+    [SerializeField] protected Vector2 knockbackDirection;
+    [SerializeField] protected float knockbackDuration;
+    protected bool isKnock;
+
     [Header("Collision info")]
     public Transform attackCheck;
     public float attackCheckRadius;
@@ -40,7 +45,19 @@ public class Entity : MonoBehaviour
     public virtual void Damage()
     {
         fx.StartCoroutine("FlashFX");
+        StartCoroutine("HitKnockback");
         Debug.Log("aa");
+    }
+
+    protected virtual IEnumerator HitKnockback()
+    {
+        isKnock = true;
+
+        rb.velocity = new Vector2(knockbackDirection.x * -facingDir, knockbackDirection.y);
+
+        yield return new WaitForSeconds(knockbackDuration);
+
+        isKnock = false;
     }
 
     #region Collision
@@ -56,9 +73,16 @@ public class Entity : MonoBehaviour
     }
     #endregion
     #region Velocity
-    public void SetZeroVelocity() => rb.velocity = new Vector2(0, 0);
+    public void SetZeroVelocity()
+    {
+        if (isKnock) return;
+
+        rb.velocity = new Vector2(0, 0);
+    }
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
+        if (isKnock) return;
+
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
         FlipController(_xVelocity);
     }

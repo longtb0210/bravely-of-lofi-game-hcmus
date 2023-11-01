10235892ae62723c9af44f8df1b4361218804c14
Bao <20127557@student.hcmus.edu.vn>
2023-11-02 03:25:19 +0700

    Lesson 69

--- Assets/Scripts/Skill/Sword_Skill.cs
+++ Assets/Scripts/Skill/Sword_Skill.cs
@@ -8,4 +8,12 @@ public class Sword_Skill : Skill
     [SerializeField] private GameObject swordPrefab;
     [SerializeField] private Vector2 launchDir;
     [SerializeField] private float swordGravity;
+
+    public void CreatSword()
+    {
+        GameObject newSword = Instantiate(swordPrefab, player.transform.position, transform.rotation);
+        Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
+
+        newSwordScript.SetupSword(launchDir, swordGravity);
+    }
 }

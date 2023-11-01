00393809d96baf529958c1dfa6f097f052e796c8
Bao <20127557@student.hcmus.edu.vn>
2023-11-02 02:14:01 +0700

    Lesson 67

--- Assets/Scripts/Skill/Clone_Skill.cs
+++ Assets/Scripts/Skill/Clone_Skill.cs
@@ -7,12 +7,14 @@ public class Clone_Skill : Skill
     [Header("Clone info")]
     [SerializeField] private GameObject clonePrefabs;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack; 
 
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefabs);
 
-        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }

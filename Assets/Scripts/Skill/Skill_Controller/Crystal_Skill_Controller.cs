fc9949d7a57683ef06582ad3bb57803b117968b7
Bao <20127557@student.hcmus.edu.vn>
2023-11-08 21:30:28 +0700

    Lesson 84

--- Assets/Scripts/Skill/Skill_Controller/Crystal_Skill_Controller.cs
+++ Assets/Scripts/Skill/Skill_Controller/Crystal_Skill_Controller.cs
@@ -1,22 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
+
     private float crystalExistTimer;
 
-    public void SetupCrystal(float _crystalDuration)
+    private bool canExplode;
+    private bool canMove;
+    private float moveSpeed;
+
+    private bool canGrow;
+    private float growSpeed = 5;
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed)
     {
         crystalExistTimer = _crystalDuration;
+        canExplode = _canExplode; ;
+        canMove = _canMove;
+        moveSpeed = _moveSpeed;
     }
 
     private void Update()
     {
         crystalExistTimer -= Time.deltaTime;
 
-        if(crystalExistTimer < 0)
-            SelfDestroy();
+        if (crystalExistTimer < 0)
+            FinishCrystal();
+
+        if (canGrow)
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+    }
+
+    private void AnimationExplodeEvent()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
+        }
+    }
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        }
+        else SelfDestroy();
     }
 
     public void SelfDestroy() => Destroy(gameObject);

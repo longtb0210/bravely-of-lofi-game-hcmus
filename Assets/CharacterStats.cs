c1c28f368ef281b842366111dce2e2843438191d
Bao <20127557@student.hcmus.edu.vn>
2023-11-15 15:52:14 +0700

    lesson 94

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -1,13 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength;   // increase damge
+    public Stat agility;    //increase evasion
+    public Stat intelligence;   //increase magic damage
+    public Stat vitality;   //increase health
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+
+    public Stat damage;
 
     [SerializeField] public float currentHealth;
 
@@ -18,8 +25,12 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamge(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamge = damage.GetValue() + strength.GetValue();
 
+        totalDamge = CheckTargetArmor(_targetStats, totalDamge);
         _targetStats.TakeDamge(totalDamge);
     }
 
@@ -33,6 +44,26 @@ public class CharacterStats : MonoBehaviour
 
     protected virtual void Die()
     {
-        
+
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamge)
+    {
+        totalDamge -= _targetStats.armor.GetValue();
+        totalDamge = Mathf.Clamp(totalDamge, 0, int.MaxValue);
+        return totalDamge;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            Debug.Log("ATTACK AVOIDED");
+            return true;
+
+        }
+        return false;
     }
 }
